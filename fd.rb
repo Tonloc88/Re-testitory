7d7049d24fb25817323a2b95875cf80877e6d596
Tonloc88 <tonyyoungdesigns@gmail.com>
2016-06-16 17:30:03 -0400

    F Day

---
+++ fd.rb
@@ -0,0 +1,12 @@
+ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+#generated with .split('').shuffle.join
+ENCODING = "MOhqm0PnycUZeLdK8YvDCgNfb7FJtiHT52BrxoAkas9RWlXpEujSGI64VzQ31w"
+
+def encode(text)
+    return text.tr(ALPHABET, ENCODING)
+end
+
+def decode(text)
+    return text.tr(ENCODING, ALPHABET)
+end
